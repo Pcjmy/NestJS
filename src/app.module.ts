f57b600cc88169f6f5682f5d2b597f144533e7a6
Pcjmy <1716045191@qq.com>
2023-09-29 21:36:47 +0800

    feat: 配置文件参数验证

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import Configuration from 'configuration';
+import * as Joi from 'joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [Configuration],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_PORT: Joi.number().default(3306),
+      }),
     }),
     UserModule,
   ],
