129b1b85f6bb5c180a0588c698e91b9af2ebf6bb
Pcjmy <1716045191@qq.com>
2023-09-29 17:20:59 +0800

    feat: config

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ConfigService } from '@nestjs/config';
+import { ConfigEnum } from 'src/enum/config.enum';
 
 @Controller('user')
 export class UserController {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private configService: ConfigService,
+  ) {}
 
   @Get()
   getUsers(): any {
+    const db = this.configService.get(ConfigEnum.DB);
+    const host = this.configService.get(ConfigEnum.DB_HOST);
     return this.userService.getUsers();
   }
 }
