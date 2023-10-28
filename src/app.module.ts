d40c849f996e40a1cb8a5c4cb12f033d05c54509
Pcjmy <1716045191@qq.com>
2023-10-28 17:20:00 +0800

    feat: 类型校验

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigEnum } from 'src/enum/config.enum';
 import Configuration from 'configuration';
 import * as Joi from 'joi';
@@ -19,35 +19,31 @@ const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
           .valid('development', 'production')
           .default('development'),
         DB_PORT: Joi.number().default(3306),
+        DB_HOST: Joi.string().ip(),
+        DB_TYPE: Joi.string().valid('mysql', 'postgres'),
+        DB_DATABASE: Joi.string().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_SYNC: Joi.boolean().default(false),
       }),
     }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get(ConfigEnum.DB_HOST),
-        port: configService.get(ConfigEnum.DB_PORT),
-        username: configService.get(ConfigEnum.DB_USERNAME),
-        password: configService.get(ConfigEnum.DB_PASSWORD),
-        database: configService.get(ConfigEnum.DB_DATABASE),
-        entities: [],
-        // 同步本地的schema与数据库 -> 初始化的时候去使用
-        synchronize: configService.get(ConfigEnum.DB_SYNC),
-        logging: ['error'],
-      }),
+      useFactory: (configService: ConfigService) =>
+        ({
+          type: configService.get(ConfigEnum.DB_TYPE),
+          host: configService.get(ConfigEnum.DB_HOST),
+          port: configService.get(ConfigEnum.DB_PORT),
+          username: configService.get(ConfigEnum.DB_USERNAME),
+          password: configService.get(ConfigEnum.DB_PASSWORD),
+          database: configService.get(ConfigEnum.DB_DATABASE),
+          entities: [],
+          // 同步本地的schema与数据库 -> 初始化的时候去使用
+          synchronize: configService.get(ConfigEnum.DB_SYNC),
+          logging: ['error'],
+        }) as TypeOrmModuleOptions,
     }),
-    // TypeOrmModule.forRoot({
-    //   type: 'mysql',
-    //   host: 'localhost',
-    //   port: 3090,
-    //   username: 'root',
-    //   password: 'example',
-    //   database: 'testdb',
-    //   entities: [],
-    //   synchronize: true,
-    //   logging: ['error'],
-    // }),
     UserModule,
   ],
   controllers: [],
