b7d6ff5d6e0125ecf84d8d66c826a2012b1aeb77
Pcjmy <1716045191@qq.com>
2023-10-28 16:58:39 +0800

    feat: 环境配置

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,18 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigEnum } from 'src/enum/config.enum';
 import Configuration from 'configuration';
 import * as Joi from 'joi';
-import { TypeOrmModule } from '@nestjs/typeorm';
+
+const envFilePath = `.env.${process.env.NODE_ENV || `development`}`;
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath,
       load: [Configuration],
       validationSchema: Joi.object({
         NODE_ENV: Joi.string()
@@ -17,17 +21,33 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         DB_PORT: Joi.number().default(3306),
       }),
     }),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3090,
-      username: 'root',
-      password: 'example',
-      database: 'testdb',
-      entities: [],
-      synchronize: true,
-      logging: ['error'],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get(ConfigEnum.DB_HOST),
+        port: configService.get(ConfigEnum.DB_PORT),
+        username: configService.get(ConfigEnum.DB_USERNAME),
+        password: configService.get(ConfigEnum.DB_PASSWORD),
+        database: configService.get(ConfigEnum.DB_DATABASE),
+        entities: [],
+        // 同步本地的schema与数据库 -> 初始化的时候去使用
+        synchronize: configService.get(ConfigEnum.DB_SYNC),
+        logging: ['error'],
+      }),
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3090,
+    //   username: 'root',
+    //   password: 'example',
+    //   database: 'testdb',
+    //   entities: [],
+    //   synchronize: true,
+    //   logging: ['error'],
+    // }),
     UserModule,
   ],
   controllers: [],
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ConfigService } from '@nestjs/config';
-import { ConfigEnum } from 'src/enum/config.enum';
 
 @Controller('user')
 export class UserController {
